8de7dc949374ade01e6e56d809ea2488547fbd9b
burak <burak.c.gungor@gmail.com>
2025-06-17 18:51:48 +0300

    Add file management

---
+++ Files/Program.cs
@@ -0,0 +1,33 @@
+﻿//Dosya yazma ve Ekleme
+
+// Dosya Ekleme
+File.WriteAllText("deneme2.txt", "mrb");
+File.WriteAllText("deneme.txt", "mrb");
+
+//içeriği günceller
+using (StreamWriter sw = File.CreateText("deneme.txt"))
+{
+    sw.WriteLine("merhaba");
+    sw.WriteLine("bcg");
+    sw.WriteLine("433");
+}
+
+//var olan içeriğe yeni bilgi ekler
+using (StreamWriter sw = File.AppendText("deneme.txt"))
+{
+    sw.WriteLine("Selam");
+    sw.WriteLine("btk");
+    sw.WriteLine("4231");
+}
+
+//Dosya okuma
+
+// StreamReader sr = File.OpenText("deneme.txt");
+// var s = "";
+// while ((s = sr.ReadLine()) != null)
+// {
+//     System.Console.WriteLine(s);
+// }
+
+string sonuc = File.ReadAllText("deneme.txt");
+System.Console.WriteLine(sonuc);
