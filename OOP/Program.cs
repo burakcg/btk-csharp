6ab5f2994c7dc31dea53c67a20fbf65076c3c364
burak <burak.c.gungor@gmail.com>
2025-06-21 16:41:35 +0300

    Add OOP

---
+++ OOP/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player ply1 = new Player();
+            ply1.PlayerNo = "45";
+            ply1.Ad = "Victor";
+            ply1.Soyad = "Osimhen";
+            ply1.Takim = "GS";
+
+            Player ply2 = new Player();
+            ply2.PlayerNo = "9";
+            ply2.Ad = "Mauro";
+            ply2.Soyad = "İcardi";
+            ply2.Takim = "GS";
+
+            Player ply3 = new Player();
+            ply3.PlayerNo = "34";
+            ply3.Ad = "Lucas";
+            ply3.Soyad = "Torreira";
+            ply3.Takim = "GS";
+
+            Player[] players = new Player[3];
+            players[0] = ply1;
+            players[1] = ply2;
+            players[2] = ply3;
+
+            foreach (var Player in players)
+            {
+                System.Console.WriteLine($"{Player.PlayerNo} numaralı oyuncunun adı {Player.Ad} {Player.Soyad} ve takımı {Player.Takim}'dır.");   
+            }
+        }
+    }
+
+    class Player
+    {
+        //propert => string, int
+        public string? PlayerNo { get; set; }
+        public string? Ad { get; set; }
+        public string? Soyad { get; set; }
+        public string? Takim { get; set; }
+
+    }
+
+
+}
---
+++ OOPsorucevap/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace OOPsorucevap
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var soru1 = new Soru(1, "Hangisi programlama dili değildir?", new string[4] { "Python", "C#", "Java", "HTML" }, "HTML");
+            var soru2 = new Soru(2, "Hangisi en popüler programlama dilidir?", new string[4] { "Python", "C#", "Java", "HTML" }, "C#");
+            var soru3 = new Soru(3, "Hangisi en popüler web programlama platformu değildir?", new string[4] { "Python", "Asp.net", "Spring", "Django" }, "Python");
+           
+            var sorular = new Soru[] { soru1, soru2, soru3 };
+            foreach (var soru in sorular)
+            {
+                System.Console.WriteLine(soru.soruYazdir());
+
+                //kullanıcıdan cevap al
+                System.Console.Write("cevabınız:");
+                var cevap = Console.ReadLine();
+                if (soru.cevapKontrol(cevap))
+                {
+                    System.Console.WriteLine("doğru");
+                }
+                else
+                {
+                    System.Console.WriteLine("yanlış");
+                }
+            }
+           
+        }
+
+
+    }
+
+    class Soru
+    {
+        public Soru(int soruId, string soruMetni, string[] secenekler, string cevap)
+        {
+            this.SoruId = soruId;
+            this.SoruMetni = soruMetni;
+            this.Secenekler = secenekler;
+            this.Cevap = cevap;
+        }
+        private int SoruId { get; set; }
+        private string SoruMetni { get; set; }
+        private string[] Secenekler { get; set; }
+        private string Cevap { get; set; }
+
+
+        public bool cevapKontrol(string cevap)
+        {
+            return this.Cevap.ToLower() == cevap.ToLower();
+        }
+        public string soruYazdir()
+        {
+            string soru = "";
+            soru += this.SoruMetni + "\n";
+            foreach (var secenek in Secenekler)
+            {
+                soru += secenek + " ";
+            }
+            return soru;
+        }
+       
+    }
+    
+   
+}
