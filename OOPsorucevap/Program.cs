09e9faee1529893e91efd22a7e6b32764d926807
burak <burak.c.gungor@gmail.com>
2025-06-22 16:24:47 +0300

    Change generic list

--- OOPsorucevap/Program.cs
+++ OOPsorucevap/Program.cs
@@ -6,11 +6,17 @@
         static void Main(string[] args)
         {
 
+
             var soru1 = new Soru(1, "Hangisi programlama dili değildir?", new string[4] { "Python", "C#", "Java", "HTML" }, "HTML");
             var soru2 = new Soru(2, "Hangisi en popüler programlama dilidir?", new string[4] { "Python", "C#", "Java", "HTML" }, "C#");
             var soru3 = new Soru(3, "Hangisi en popüler web programlama platformu değildir?", new string[4] { "Python", "Asp.net", "Spring", "Django" }, "Python");
-           
-            var sorular = new Soru[] { soru1, soru2, soru3 };
+
+            // var sorular = new Soru[] { soru1, soru2, soru3 }; //array list
+
+            List<Soru> sorular = new List<Soru>();              // generic list
+            sorular.Add(soru1);
+            sorular.Add(soru2);
+            sorular.Add(soru3);
             foreach (var soru in sorular)
             {
                 System.Console.WriteLine(soru.soruYazdir());
@@ -27,7 +33,18 @@
                     System.Console.WriteLine("yanlış");
                 }
             }
-           
+
+            Dictionary<int, string> cevaplar = new Dictionary<int, string>();  //Dictionary
+            cevaplar.Add(soru1.cevapIdYazdir(), soru1.cevapYazdir());
+            cevaplar.Add(soru2.cevapIdYazdir(), soru2.cevapYazdir());
+            cevaplar.Add(soru3.cevapIdYazdir(), soru3.cevapYazdir());
+
+            //update
+            cevaplar[soru2.cevapIdYazdir()] = "C Sharp";
+            foreach (KeyValuePair<int, string> c in cevaplar)
+            {
+                System.Console.WriteLine(c.Key + " " + c.Value);
+            }
         }
 
 
@@ -62,7 +79,16 @@
             }
             return soru;
         }
-       
+
+        public string cevapYazdir()
+        {
+            string c = this.Cevap;
+            return c;
+        }
+        public int cevapIdYazdir()
+        {
+            return this.SoruId;
+        }
     }
     
    
